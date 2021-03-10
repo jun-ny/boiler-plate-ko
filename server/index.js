4b784a693ee0edf9475d4cbbbc1b1bce3bf11827
junhyeong <whitelaw7@naver.com>
2021-03-10 21:34:46 +0900

    210310

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
@@ -22,6 +21,11 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요~ㅎㅎ 새해복 많이 받으세요')
+})
+
+app.get('/api/hello', (req, res) => {
+  
+  res.send("안녕하세요 ~")
 })
 
 app.post('/api/users/register', (req, res) => {
@@ -101,6 +105,7 @@ app.get('/api/users/logout', auth, (req, res) => {
 
 }) 
 
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
